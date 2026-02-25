fa1f8f5c5580eb17bb0a8ba9f9f210cd433956c0
RubelMozumder <32923026+RubelMozumder@users.noreply.github.com>
2026-02-25 14:29:07 +0100

    Revert "Fix issue2"

--- tests/north_tools/test_north_tools.py
+++ tests/north_tools/test_north_tools.py
@@ -1,10 +1,10 @@
 def test_importing_north_tool():
     # this will raise an exception if pydantic model validation fails for the north tool
-    from nomad_north_voila.north_tools.voila import (
+    from nomad_north_voila.north_tools.voila_north_tool import (
         north_tool_entry_point,
     )
 
     assert (
-        north_tool_entry_point.id_url_safe == 'north_tool_voila'
-        or north_tool_entry_point.id == 'nomad-north-voila'
+        north_tool_entry_point.id_url_safe == 'voila_north_tool'
+        or north_tool_entry_point.id_url_safe == 'nomad-north-nomad-north-voila'
     ), 'NORTHtool entry point has incorrect id or id_url_safe'
